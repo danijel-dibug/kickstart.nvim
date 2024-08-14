68d62daa6a45297f020bf3715536f2dfcd1b8f7d
Dani <danijel.matek@dibug-solutions.hr>
2024-08-14 12:10:40 +0200

    Add keymap to move visual selected block

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set('n', 'q', '<nop>')
+
+-- move visual selected block or line
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 return {
   'tpope/vim-fugitive',
   {
