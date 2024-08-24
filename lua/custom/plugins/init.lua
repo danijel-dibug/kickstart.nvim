fd4c428e302e6f6505f471af3c5a74afa0dc3dc6
Dani <danijel.matek@dibug-solutions.hr>
2024-08-24 07:03:40 +0200

    Change to relative line numbers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,12 +3,16 @@
 --
 -- See the kickstart.nvim README for more information
 
+-- vim.o.guicursor = 'n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+
 vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('n', 'q', '<nop>')
 
 -- move visual selected block or line
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.wo.relativenumber = true
 
 return {
   'tpope/vim-fugitive',
